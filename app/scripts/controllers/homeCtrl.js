3703dabc1a84728479d2fc35b2a7d525d433bd48
Sharmi <rai.sharmistha@gmail.com>
2016-09-06 12:29:23 -0400

    trying to fix buttons

--- app/scripts/controllers/homeCtrl.js
+++ app/scripts/controllers/homeCtrl.js
@@ -1,10 +1,10 @@
 (function() {
-  function homeCtrl($scope, $firebaseObject) {
+  function HomeCtrl($scope, $firebaseObject) {
     var ref = firebase.database().ref();
     $scope.data = $firebaseObject(ref);
   }
 
   angular
     .module('pomodoro')
-    .controller('homeCtrl', ['$scope', '$firebaseObject', homeCtrl]);
+    .controller('HomeCtrl', ['$scope', '$firebaseObject', HomeCtrl]);
 })();
