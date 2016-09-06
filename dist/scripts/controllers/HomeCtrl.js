ef76eefb4b6c5bea0b5bc0c2d56192c26c723fe2
Sharmi <rai.sharmistha@gmail.com>
2016-09-06 16:35:07 -0400

    complete start and reset session

--- dist/scripts/controllers/HomeCtrl.js
+++ dist/scripts/controllers/HomeCtrl.js
@@ -1,9 +1,32 @@
 (function() {
-  function HomeCtrl($scope) {
+  function HomeCtrl($interval) {
+// Total time in seconds (25 min)
+    this.count = 1500;
 
-  }
+    var interval = null;
+
+  //Start button logic
+    this.start = function() {
+      if(interval === null) {
+        interval = $interval(countdown.bind(this), 1000);
+      }
+    };
+
+  // Later refactor to toggle between pause and play + extra button to reset?
+
+  //Reset button logic
+    this.reset = function() {
+      $interval.cancel(interval);
+      this.count = 1500;
+      interval = null;
+    };
+
+    function countdown() {
+      this.count--;
+    };
+};
 
   angular
     .module('pomodoro')
-    .controller('HomeCtrl', ['$scope', HomeCtrl]);
+    .controller('HomeCtrl', ['$interval', HomeCtrl]);
 })();
